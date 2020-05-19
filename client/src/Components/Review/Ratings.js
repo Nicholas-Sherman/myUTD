25c5fa4bddc2b084948488816d3b9c93eb47f956
yukihan99 <yukihan99@gmail.com>
2020-05-18 19:19:59 -0500

    added state to tags

--- client/src/Components/Review/Ratings.js
+++ client/src/Components/Review/Ratings.js
@@ -5,7 +5,7 @@ import { FaStar } from "react-icons/fa"
 class Ratings extends Component {
     constructor(props) {
         super(props)
-        this.state = {selectedOption: "1"};
+        this.state = {input: ' '};
         this.optionChange = this.optionChange.bind(this);
     }
 
@@ -20,35 +20,35 @@ render () {
                 type = "radio" 
                 id = "rating1" 
                 value = "1" 
-                checked={this.state.selectedOption == "1"}
+                checked={this.state.selectedOption === "1"}
                 onChange={this.optionChange}/>
                 <label for="rating1"><FaStar size = "37px"/>Awful</label>
             <input 
                 type = "radio" 
                 id = "rating2" 
                 value = "2" 
-                checked={this.state.selectedOption == "2"} 
+                checked={this.state.selectedOption === "2"} 
                 onChange={this.optionChange}/>
                 <label for="rating2"><FaStar size = "37px"/>Poor</label>
             <input 
                 type = "radio" 
                 id = "rating3" 
                 value = "3" 
-                checked={this.state.selectedOption == "3"} 
+                checked={this.state.selectedOption === "3"} 
                 onChange={this.optionChange}/>
                 <label for="rating3"><FaStar size = "37px"/>Fair</label>
             <input 
                 type = "radio" 
                 id = "rating4" 
                 value = "4" 
-                checked={this.state.selectedOption == "4"}
+                checked={this.state.selectedOption === "4"}
                 onChange={this.optionChange}/>
                 <label for="rating4"><FaStar size = "37px"/>Good</label>
             <input 
                 type = "radio" 
                 id = "rating5" 
                 value = "5" 
-                checked={this.state.selectedOption == "5"} 
+                checked={this.state.selectedOption === "5"} 
                 onChange={this.optionChange}/>
                 <label for="rating5"><FaStar size = "37px"/>Great</label>
         </div>
